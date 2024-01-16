5cf450dd595c5efe6ec34e58786b8c4ae81528bf
“2024-DEV2-006” <ios06developer@gmail.com>
2024-01-16 13:17:32 +0530

    Add check for second lamp off scenario

---
+++ BerlinClock/Model/BerlinClockModel.swift
@@ -0,0 +1,26 @@
+//
+//  BerlinClockModel.swift
+//  BerlinClock
+//
+//  Created by 2024-DEV2-006 on 16/01/24.
+//
+
+import Foundation
+
+struct BerlinClockModel {
+    
+    func convertToBerlinTime(_ time: String) -> String {
+        
+        let components = time.components(separatedBy: ":")
+        guard components.count == 3, let seconds = Int(components[2]) else {
+            return "Invalid time format"
+        }
+        
+        return checkSecondsLamp(seconds:seconds)
+    }
+    
+    func checkSecondsLamp(seconds: Int) -> String{
+        return "O"
+    }
+    
+}
--- BerlinClockTests/BerlinClockTests.swift
+++ BerlinClockTests/BerlinClockTests.swift
@@ -9,4 +9,10 @@ import XCTest
 @testable import BerlinClock
 
 final class BerlinClockTests: XCTestCase {
+    
+    let berlinClock = BerlinClockModel()
+
+      func testSecondsLamp_Off(){
+          XCTAssertEqual("O", berlinClock.convertToBerlinTime("00:00:01"))
+      }
 }
