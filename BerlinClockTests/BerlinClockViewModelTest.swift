3237ccf126b5580ce69b132c6a44df61bc2076b3
“2024-DEV2-006” <ios06developer@gmail.com>
2024-01-17 15:44:39 +0530

    Add check for view model seconds lamp On and all minutes lamp On scenario

--- BerlinClockTests/BerlinClockViewModelTest.swift
+++ BerlinClockTests/BerlinClockViewModelTest.swift
@@ -25,6 +25,30 @@ final class BerlinClockViewModelTest: XCTestCase {
         XCTAssertEqual(updatedLamps.fiveMinutes, [.off, .off, .off, .off, .off, .off, .off, .off, .off, .off, .off])
         XCTAssertEqual(updatedLamps.oneMinutes, [.off, .off, .off, .off])
     }
+    func testLamp_SecondsOn(){
+        let date = Date.getDateFrom(hour: 00, minute: 00, second: 00)
+        
+        berlinClockViewModel.convertToBerlinTime(date)
+        let updatedLamps = berlinClockViewModel.berlinClockLamps
+        
+        XCTAssertEqual(updatedLamps.seconds, .yellow)
+        XCTAssertEqual(updatedLamps.fiveHours, [.off, .off, .off, .off])
+        XCTAssertEqual(updatedLamps.oneHours, [.off, .off, .off, .off])
+        XCTAssertEqual(updatedLamps.fiveMinutes, [.off, .off, .off, .off, .off, .off, .off, .off, .off, .off, .off])
+        XCTAssertEqual(updatedLamps.oneMinutes, [.off, .off, .off, .off])
+    }
+    func testLamp_AllMinutesOn(){
+        let date = Date.getDateFrom(hour: 00, minute: 59, second: 01)
+        
+        berlinClockViewModel.convertToBerlinTime(date)
+        let updatedLamps = berlinClockViewModel.berlinClockLamps
+        
+        XCTAssertEqual(updatedLamps.seconds, .off)
+        XCTAssertEqual(updatedLamps.fiveHours, [.off, .off, .off, .off])
+        XCTAssertEqual(updatedLamps.oneHours, [.off, .off, .off, .off])
+        XCTAssertEqual(updatedLamps.fiveMinutes, [.yellow, .yellow, .red, .yellow, .yellow, .red, .yellow, .yellow, .red, .yellow, .yellow])
+        XCTAssertEqual(updatedLamps.oneMinutes, [.yellow, .yellow, .yellow, .yellow])
+    }
 
 }
     
